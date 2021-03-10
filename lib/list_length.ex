c0673eb807a0fa8fee55b50e310fb792fa6e2e3f
Bruno <brf147@gmail.com>
2021-03-10 08:42:28 -0400

    Ignite-Elixir-Challenge-01

---
+++ lib/list_length.ex
@@ -0,0 +1,11 @@
+defmodule ListLength do
+  def call(list), do: get_length(list, 0)
+
+  def get_length([], acc), do: acc
+
+  def get_length([_head | tail], acc) do
+    acc = acc + 1
+
+    get_length(tail, acc)
+  end
+end
